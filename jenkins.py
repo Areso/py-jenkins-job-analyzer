49b5927124ebb4b12f4e9c8a65b96eff3f7a32a1
Anton Gladyshev <returnstrike@yandex.ru>
2021-08-20 18:57:00 +0300

    fix types

--- jenkins.py
+++ jenkins.py
@@ -5,6 +5,6 @@ jenkins_address = ''
 job_name = ''
 address = jenkins_address+'/'+job_name
 job_id  = 0
-address = address+'/'+job_id+'/api/python?pretty=true'
+address = address+'/'+str(job_id)+'/api/python?pretty=true'
 r = requests.get(address)
 print(r)
