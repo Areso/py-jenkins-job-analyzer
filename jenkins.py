325a6b4871188caa333dbb1e5b3476b1960ecd70
Anton Gladyshev <returnstrike@yandex.ru>
2021-08-20 21:16:44 +0300

    incoming params array

--- jenkins.py
+++ jenkins.py
@@ -12,6 +12,6 @@ job_name = ''
 address  = schema+username+':'+token+'@'+jnk_address+'/'+job_name
 job_id   = 0
 
-address  = address+'/'+str(job_id)+'/api/python?pretty=true'
-r = requests.get(address, verify=ssl_sign_enforce)
-print(r)
+address  = address+'/'+str(job_id)+'/api/json?pretty=true'
+r = requests.get(address, verify=ssl_sign_enforce).json()
+incoming_params_arr = r['actions'][0]['parameters']
