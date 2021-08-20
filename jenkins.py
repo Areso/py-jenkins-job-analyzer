e4b49411ad7c07e2ab9eeb98df566d37c6671643
Anton Gladyshev <returnstrike@yandex.ru>
2021-08-20 18:21:28 +0300

    grabbing a page!

---
+++ jenkins.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import requests
+
+jenkins_address = ''
+job_name = ''
+address = jenkins_address+'/'+job_name
+job_id  = 0
+address = address+'/'+job_id+'/api/python?pretty=true'
+r = requests.get(address)
+print(r)
