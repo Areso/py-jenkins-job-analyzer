70d420becf2b310c9abac5fea9ada6e778a20e91
Anton Gladyshev <returnstrike@yandex.ru>
2021-08-20 19:55:46 +0300

    with token

--- jenkins.py
+++ jenkins.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 import requests
 
-jenkins_address = ''
+schema   = 'https://'
+jnk_address = ''
+username = ''
+token    = ''
+
 job_name = ''
-address = jenkins_address+'/'+job_name
-job_id  = 0
-address = address+'/'+str(job_id)+'/api/python?pretty=true'
+address  = schema+username+':'+token+'@'+jnk_address+'/'+job_name
+job_id   = 0
+
+address  = address+'/'+str(job_id)+'/api/python?pretty=true'
 r = requests.get(address, verify=False)
 print(r)
