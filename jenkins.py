73a6b05357363a4289fab622edce955890095538
Anton Gladyshev <returnstrike@yandex.ru>
2021-08-21 23:47:19 +0300

    changed to dict

--- jenkins.py
+++ jenkins.py
@@ -5,11 +5,11 @@ import json
 
 
 def bigf(options):
-    address          = options.schema+"://"+options.username+':'+options.token
-    address          = address+'@'+options.jnk_address+'/'+options.job_name[0]
+    address          = options['schema']+'://'+options['username']+':'+options['token']
+    address          = address+'@'+options['jnk_address']+'/'+options['job_name'[0]]
  
-    address    = address+'/'+str(options.job_id)+'/api/json?pretty=true'
-    r          = requests.get(address, verify=options.ssl_enforce).json()
+    address    = address+'/'+str(options['job_id'])+'/api/json?pretty=true'
+    r          = requests.get(address, verify=bool(options['ssl_enforce'])).json()
     params     = r['actions'][0]['parameters']
     params_new = {}
     for param in params:
