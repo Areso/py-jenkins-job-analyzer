41d69743af1cb0857c3b4b7c16c446c3e37d3f1a
Anton Gladyshev <returnstrike@yandex.ru>
2021-08-23 20:41:04 +0300

    writing a job data to DB

--- jenkins.py
+++ jenkins.py
@@ -5,9 +5,6 @@ import psycopg2
 import sys
 
 def bigf(options):
-    address          = options['schema']+'://'+options['username']+':'+options['token']
-    address          = address+'@'+options['jnk_address']+'/'+options['job_names'][0]
-    address    = address+'/'+str(options['job_id'])+'/api/json?pretty=true'
     ssl_verify = bool(options['ssl_enforce'])
     #DB connect
     try:
@@ -31,31 +28,79 @@ def bigf(options):
     cursor.execute("SELECT * FROM job_defs WHERE job_link=%(job_name)s",
                    {"job_name": options["job_names"][0]})
     records = cursor.fetchall()
-    #INSERTING
+    #INSERTING IF DOESN'T EXIST
     if len(records)==0:
-        test = cursor.execute("INSERT INTO job_defs (job_link) VALUES (%(job_name)s) RETURNING id",
+        cursor.execute("INSERT INTO job_defs (job_link) VALUES (%(job_name)s) RETURNING id",
                    {"job_name": options["job_names"][0]})
         rec_id = cursor.fetchall()
-        print("---returnning from cursor execute---")
-        print(test)
-        print("---rec id - cursor from fetchall---")
-        print(rec_id)
-    r          = requests.get(address, verify=ssl_verify).json()
-    params     = r['actions'][0]['parameters']
-    params_new = {}
-    for param in params:
-        param_key    = param['name']
-        param_value  = param['value']
-        params_new[param_key] = param_value
-    display_name = r['displayName']
-    full_job_url = r['url']
-    job_causer   = r['actions'][1]['causes']
-    job_duration = r['duration']
-    job_timestamp= r['timestamp']
-    job_result   = r['result']
-    job_executor = r['executor']
-    job_timings  = r['actions'][2]
-    job_git_dets = r['actions'][6]
+        conn.commit()
+        job_ref_id = rec_id[0][0]
+    else:
+        job_ref_id = records[0][0]
+    print("job_ref_id")
+    print(job_ref_id)
+    #STARTING GATHERING THE JOB RUNS
+    address          = options['schema']+'://'+options['username']+':'+options['token']
+    address          = address+'@'+options['jnk_address']+'/'+options['job_names'][0]
+    for i in range(options['job_start_id'], options['job_start_id']):
+        address    = address+'/'+str(i)+'/api/json?pretty=true'
+        r          = requests.get(address, verify=ssl_verify).json()
+        params     = r['actions'][0]['parameters']
+        params_new = {}
+        for param in params:
+            param_key    = param['name']
+            param_value  = param['value']
+            params_new[param_key] = param_value
+        display_name = r['displayName']
+        full_job_url = r['url']
+        job_causer   = r['actions'][1]['causes']
+        job_duration = r['duration']
+        job_timestamp= r['timestamp']
+        job_result   = r['result']
+        job_executor = r['executor']
+        job_timings  = r['actions'][2]
+        job_git_dets = r['actions'][6]
+        #INSERTING THE RESULT
+        cursor.execute("""INSERT INTO job_runs (job_id, 
+                                                job_run_id,
+                                                params,
+                                                display_name,
+                                                full_job_url,
+                                                job_causer,
+                                                job_duration,
+                                                job_timestamp,
+                                                job_result,
+                                                job_timings,
+                                                job_git_dets)
+                                                 VALUES 
+                                                (%(job_id)s,
+                                                 %(job_run_id)s,
+                                                 %(display_name)s,
+                                                 %(full_job_url)s,
+                                                 %(job_causer)s,
+                                                 %(job_duration)s,
+                                                 %(job_timestamp)s,
+                                                 %(job_result)s,
+                                                 %(job_timings)s,
+                                                 %(job_git_dets)s
+                                                )""", {
+                                                "job_id": ob_ref_id,
+                                                "job_run_id": i,
+                                                "params": params_new,
+                                                "display_name": display_name,
+                                                "full_job_url": full_job_url,
+                                                "job_causer": job_causer,
+                                                "job_duration": job_duration,
+                                                "job_result": job_result,
+                                                "job_executor": job_executor,
+                                                "job_timings": job_timings,
+                                                "job_git_dets": job_git_dets
+                                                })
+        print(cursor.query)
+        conn.commit()
+
+    cursor.close()
+    conn.close()
 
 
 if __name__ == "__main__":
